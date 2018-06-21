d366e27c5649f5ff4cc43f1b5161def758b6840e
Asia Antczak <aasiaantczak@gmail.com>
2018-06-21 10:11:07 +0100

    Add test for update quality of item with 0 sellIn value

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,18 +2,25 @@ require 'gilded_rose'
 
 describe GildedRose do
 
-  let(:items) {[Item.new("foo", 0, 0)]}
+  let(:items) { [Item.new(name="+5 Dexterity Vest", sell_in=1, quality=0),
+                 Item.new(name="+5 Dexterity Vest", sell_in=0, quality=4)]}
   let(:gilded_rose) { described_class.new(items) }
 
   describe '#update_quality' do
     it 'does not change the name' do
       gilded_rose.update_quality()
-      expect(items[0].name).to eq "foo"
+      expect(items[0].name).to eq "+5 Dexterity Vest"
     end
 
     it 'does not lower the quality below 0' do
       gilded_rose.update_quality()
       expect(items[0].quality).to eq 0
     end
+
+    it 'degrades quality twice as fast for items with sellIn 0' do
+      gilded_rose.update_quality()
+      expect(items[1].quality).to eq 2
+    end
+
   end
 end
