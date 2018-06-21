b56bc149875d2c3f7ca1027280fade8cb7c91398
Asia Antczak <aasiaantczak@gmail.com>
2018-06-21 10:03:41 +0100

    Add test for update quality of item with 0 quality

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,12 +2,18 @@ require 'gilded_rose'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+  let(:items) {[Item.new("foo", 0, 0)]}
+  let(:gilded_rose) { described_class.new(items) }
+
+  describe '#update_quality' do
+    it 'does not change the name' do
+      gilded_rose.update_quality()
       expect(items[0].name).to eq "foo"
     end
-  end
 
+    it 'does not lower the quality below 0' do
+      gilded_rose.update_quality()
+      expect(items[0].quality).to eq 0
+    end
+  end
 end
