4eb8a25fa47176bfccdd7614db182861a18b89f3
Asia Antczak <aasiaantczak@gmail.com>
2018-06-21 11:21:26 +0100

    Add tests for updating quality of Backstage passes

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,7 +6,12 @@ describe GildedRose do
                  Item.new(name="+5 Dexterity Vest", sell_in=0, quality=4),
                  Item.new(name="Aged Brie", sell_in=2, quality=0),
                  Item.new(name="Aged Brie", sell_in=2, quality=49),
-                 Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=40)]}
+                 Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=40),
+                 Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=11, quality=40),
+                 Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=40),
+                 Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=40),
+                 Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=0, quality=40)] }
+
   let(:gilded_rose) { described_class.new(items) }
 
   describe '#update_quality' do
@@ -44,6 +49,26 @@ describe GildedRose do
     it 'does not change the sell_in value of Sulfuras' do
       gilded_rose.update_quality()
       expect(items[4].sell_in).to eq 10
+    end
+
+    it 'increases the quality of Backstage passes by 2 when sell_in between 10 and 6' do
+      gilded_rose.update_quality()
+      expect(items[6].quality).to eq 42
+    end
+
+    it 'increases the quality of Backstage passes by 1 when sell_in is over 10' do
+      gilded_rose.update_quality()
+      expect(items[5].quality).to eq 41
+    end
+
+    it 'increases the quality of Backstage passes by 3 when sell_in between 5 and 1' do
+      gilded_rose.update_quality()
+      expect(items[7].quality).to eq 43
+    end
+
+    it 'descreases the quality of Backstage passes to 0 when sell_in 0' do
+      gilded_rose.update_quality()
+      expect(items[8].quality).to eq 0
     end
 
   end
