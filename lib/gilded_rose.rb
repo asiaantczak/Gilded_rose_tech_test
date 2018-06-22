fefbddca68a7f3e474426d6318ba2b7eb1e39d06
Asia Antczak <aasiaantczak@gmail.com>
2018-06-22 17:40:18 +0100

    Minor refactoring

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -19,7 +19,7 @@ private
 
 def update_aged_brie(item)
   if item.name == "Aged Brie"
-    max_quality(item, 1)
+    increase_quality(item, 1)
     item.sell_in <= 0 ? item.sell_in : item.sell_in -= 1
   end
 end
@@ -34,11 +34,11 @@ end
 def update_backstage_passes(item)
   if item.name == "Backstage passes to a TAFKAL80ETC concert"
     if item.sell_in > 10
-      max_quality(item, 1)
+      increase_quality(item, 1)
     elsif item.sell_in > 5
-      max_quality(item, 2)
+      increase_quality(item, 2)
     elsif item.sell_in > 0
-      max_quality(item, 3)
+      increase_quality(item, 3)
     else
       item.quality = 0
     end
@@ -46,7 +46,7 @@ def update_backstage_passes(item)
   end
 end
 
-def max_quality(item, n)
+def increase_quality(item, n)
   n.times do
     break if item.quality == 50
     item.quality += 1
@@ -56,16 +56,16 @@ end
 def update_standard_products(item)
   if item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros" && item.name != "Aged Brie"
     if item.sell_in > 0
-      min_quality(item, 1)
+      decrease_quality(item, 1)
       item.sell_in -= 1
     else
       item.sell_in = item.sell_in
-      min_quality(item, 2)
+      decrease_quality(item, 2)
     end
   end
 end
 
-def min_quality(item, n)
+def decrease_quality(item, n)
   n.times do
     break if item.quality == 0
     item.quality -= 1
