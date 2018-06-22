2a1f0111acd0cb775fe8bda84240f6ce0fb47ab6
Asia Antczak <aasiaantczak@gmail.com>
2018-06-22 16:29:08 +0100

    Refactoring with max_quality method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -19,7 +19,7 @@ private
 
 def update_aged_brie(item)
   if item.name == "Aged Brie"
-    item.quality >= 50 ? item.quality : item.quality += 1
+    max_quality(item, 1)
     item.sell_in <= 0 ? item.sell_in : item.sell_in -= 1
   end
 end
@@ -35,9 +35,9 @@ def update_backstage_passes(item)
   if item.name == "Backstage passes to a TAFKAL80ETC concert"
     if item.sell_in > 10
       max_quality(item, 1)
-    elsif item.sell_in <= 10 && item.sell_in > 5
+    elsif item.sell_in > 5
       max_quality(item, 2)
-    elsif item.sell_in > 0 && item.sell_in <= 5
+    elsif item.sell_in > 0
       max_quality(item, 3)
     else
       item.quality = 0
@@ -56,11 +56,11 @@ end
 def update_standard_products(item)
   if item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros" && item.name != "Aged Brie"
     if item.sell_in > 0
-      item.quality <= 0 ? item.quality : min_quality(item, 1)
+      min_quality(item, 1)
       item.sell_in -= 1
     else
       item.sell_in = item.sell_in
-      item.quality <= 0 ? item.quality : min_quality(item, 2)
+      min_quality(item, 2)
     end
   end
 end
